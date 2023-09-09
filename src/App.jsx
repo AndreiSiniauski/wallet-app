2a3a3026553724d5a9d1bd89be0dc03765c9502e
Andrei Siniauski <superkenka@gmail.com>
2023-09-09 23:08:17 +0300

    07-add walletconnect

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Web3 from "web3";
 import { abi } from "./utils/contract/abiUSDT";
 import { contract } from "./utils/contract/contractUSDT";
 import { formatAddress } from "./utils/formatAddress";
+import WalletConnectProvider from "@walletconnect/web3-provider";
 
 const App = () => {
   const [address, setAddress] = useState("");
@@ -19,13 +20,13 @@ const App = () => {
             method: "wallet_requestPermissions",
             params: [
               {
-                eth_accounts: {}
-              }
-            ]
+                eth_accounts: {},
+              },
+            ],
           })
           .then(() =>
             window.ethereum.request({
-              method: "eth_requestAccounts"
+              method: "eth_requestAccounts",
             })
           );
 
@@ -41,7 +42,10 @@ const App = () => {
           const usdtBalance = await usdtContract.methods
             .balanceOf(selectedAddress)
             .call();
-          const formattedUsdtBalance = web3.utils.fromWei(usdtBalance, "ether");
+          const formattedUsdtBalance = web3.utils.fromWei(
+            usdtBalance,
+            "ether"
+          );
           const roundedUsdtBalance = parseFloat(formattedUsdtBalance).toFixed(
             3
           );
@@ -63,20 +67,63 @@ const App = () => {
     setConnected(false);
   };
 
+  const connectWalletConnect = async () => {
+    try {
+      const provider = new WalletConnectProvider({
+        rpc: {
+          56: "https://bsc-dataseed.binance.org/",
+        },
+      });
+
+      await provider.enable();
+
+      const web3 = new Web3(provider);
+
+      const accounts = await web3.eth.getAccounts();
+
+      await provider.disconnect();
+
+      if (
+        Array.isArray(accounts) &&
+        accounts.length > 0 &&
+        typeof accounts[0] === "string"
+      ) {
+        const selectedAddress = accounts[0];
+        setAddress(formatAddress(selectedAddress));
+
+        const usdtContract = new web3.eth.Contract(abi, contract);
+        const usdtBalance = await usdtContract.methods
+          .balanceOf(selectedAddress)
+          .call();
+        const formattedUsdtBalance = web3.utils.fromWei(usdtBalance, "ether");
+        const roundedUsdtBalance = parseFloat(formattedUsdtBalance).toFixed(3);
+
+        setBalance(roundedUsdtBalance);
+        setConnected(true);
+      } else {
+        console.error("Не удалось подключить WalletConnect");
+      }
+    } catch (error) {
+      console.error(error);
+    }
+    
+  };
+
   return (
-    <>
-      <div className="modal-content">
-        {connected ? (
-          <div className="content">
-            <p>Адрес кошелька: {address}</p>
-            <p>Баланс: {balance} USDT</p>
-            <button onClick={disconnectMetaMask}>Отключить MetaMask</button>
-          </div>
-        ) : (
+    <div className="modal-content">
+      {connected ? (
+        <div className="content">
+          <p>Адрес кошелька: {address}</p>
+          <p>Баланс: {balance} USDT</p>
+          <button onClick={disconnectMetaMask}>Отключить MetaMask</button>
+        </div>
+      ) : (
+        <>
           <button onClick={connectMetaMask}>Подключить MetaMask</button>
-        )}
-      </div>
-    </>
+          <button onClick={connectWalletConnect}>Подключить WalletConnect</button>
+        </>
+      )}
+    </div>
   );
 };
 
